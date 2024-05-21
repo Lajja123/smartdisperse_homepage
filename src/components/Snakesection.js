a64d324149f9f5ef803484c60b4fdabd6f3128e7
Princi <principatel1709@gmail.com>
2024-05-21 14:47:30 +0530

    pulling latest code

--- src/components/Snakesection.js
+++ src/components/Snakesection.js
@@ -3,6 +3,7 @@ import styles from "./snakesection.module.css";
 import home from "../components/homepage.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faLink, faMagnifyingGlassChart, faRectangleList, faShuffle, faShare, faUser, faWallet} from "@fortawesome/free-solid-svg-icons";
+import SnakeSVG from "@/assests/snake.svg";
 
 function Snakesection() {
   return (
@@ -71,9 +72,21 @@ function Snakesection() {
           </p>
         </article> */}
 
-        <svg width="100%" viewBox="0 0 1156 608" xmlns="http://www.w3.org/2000/svg">
-          <path className={styles.path} d="m560.30957,10.588011c0,0 438.0947,1.90476 439.04708,1.90476c0.95238,0 144.57857,-1.02912 143.80934,137.14269c-0.76923,138.17181 -116.81095,142.30859 -131.61967,143.8923c-14.80873,1.58372 -840.41472,-0.71429 -860.5941,0.71429c-20.17938,1.42858 -148.4991,6.80903 -146.83244,147.05973c1.66666,140.2507 129.52365,152.14266 129.33243,151.27321c0.19122,0.86945 815.268425,2.687632 951.42748,0" opacity="0.5" strokeWidth="7" stroke="#2567d1" fill="none"/>
-        </svg>
+<svg width="100%" viewBox="0 0 1156 608" xmlns="http://www.w3.org/2000/svg">
+  <path className={styles.path} 
+        d="m560.30957,10.588011c0,0 438.0947,1.90476 439.04708,1.90476c0.95238,0 144.57857,-1.02912 143.80934,137.14269c-0.76923,138.17181 -116.81095,142.30859 -131.61967,143.8923c-14.80873,1.58372 -840.41472,-0.71429 -860.5941,0.71429c-20.17938,1.42858 -148.4991,6.80903 -146.83244,147.05973c1.66666,140.2507 129.52365,152.14266 129.33243,151.27321c0.19122,0.86945 815.268425,2.687632 951.42748,0" 
+        opacity="0.5" 
+        strokeWidth="2" 
+        stroke="#2567d1" 
+        stroke-dasharray="10 10" 
+        fill="none"
+  />
+</svg>
+
+        {/* <svg width="100%" height="100%" viewBox="0 0 2172 1105" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1360.5 2H1897.25C2047.89 2 2170 124.114 2170 274.75V274.75C2170 425.386 2047.89 547.5 1897.25 547.5H279.5C126.241 547.5 2 671.741 2 825V825C2 978.259 126.241 1102.5 279.5 1102.5H1445.5" stroke="white" stroke-width="4" stroke-dasharray="20 20"/>
+</svg> */}
+
       </div>
     </div>
     </div>
